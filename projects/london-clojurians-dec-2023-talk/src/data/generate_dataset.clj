1188ddb06567e433d09564d464087674bc9a0210
daslu <daniel.slutsky@gmail.com>
2023-11-30 23:45:02 +0200

    initial data collection & exploration

--- projects/london-clojurians-dec-2023-talk/src/data/generate_dataset.clj
+++ projects/london-clojurians-dec-2023-talk/src/data/generate_dataset.clj
@@ -1,8 +1,25 @@
 (ns data.generate-dataset
   (:require
    [clj-http.client :as client]
-   [charred.api :as charred])
-  )
+   [charred.api :as charred]
+   [tablecloth.api :as tc]
+   [scicloj.tempfiles.api :as tempfiles]
+   [clojure.edn :as edn]
+   [again.core :as again]
+   [clojure.pprint :as pp]
+   [clojure.string :as string]))
+
+(comment
+  (tempfiles/cleanup-all-tempdirs!))
+
+(comment
+  (-> 1
+      url
+      get-page!
+      :body
+      (charred/read-json :key-fn keyword)
+      :total_count))
+
 
 (defn url [page-number]
   (str "https://api.github.com/search/repositories?q=language:clojure&order=desc&page="
@@ -10,7 +27,32 @@
        "&per_page=50"))
 
 (defn get-page! [url]
-  (client/get url {:oauth-token (System/getenv "GITHUB_OAUTH_TOKEN")}))
+  (prn [:get-page! url])
+  (again/with-retries
+    (->> 1
+         (iterate (partial * 4))
+         (take 9)
+         vec)
+    (client/get url {:oauth-token (System/getenv "GITHUB_AUTH_TOKEN")})))
+
+(def cached-resource
+  (memoize
+   (fn [url]
+     (let [path (-> ".json"
+                    tempfiles/tempfile!
+                    :path)]
+       (-> url
+           get-page!
+           (dissoc :http-client :headers)
+           pr-str
+           (->> (spit path)))
+       path))))
+
+(defn get-cached-page! [url]
+  (-> url
+      cached-resource
+      slurp
+      edn/read-string))
 
 (defn generate-ds-loop []
   (loop [result []
@@ -25,10 +67,60 @@
         result))))
 
 (defn generate-ds-iteration []
-  (iteration get-page!
+  (iteration get-cached-page!
              :initk "https://api.github.com/search/repositories?q=language:clojure&order=desc&page=1&per_page=50"
              :vf (fn [response]
                    (let [items (-> response :body (charred/read-json :key-fn keyword))]
                      {:items items
                       :count (count items)}))
              :kf (fn [response] (-> response :links :next :href))))
+
+(defn fresh-data []
+  (->> (generate-ds-iteration)
+       vec
+       time))
+
+(defn timestamp []
+  (-> (java.util.Date.)
+      pr-str
+      (subs 6)
+      read-string))
+
+(defn timestamped-filename [name ext]
+  (str "data/"
+       name
+       (timestamp)
+       "."
+       ext))
+
+(comment
+  (->> (fresh-data)
+       pr-str
+       (spit (timestamped-filename "repos" "edn"))))
+
+(defonce data
+  (-> "data/repos2023-11-30T16:13:12.877-00:00.edn"
+      slurp
+      edn/read-string))
+
+
+(->> data
+     (mapcat (comp :items :items))
+     (map (fn [repo]
+            (-> repo
+                :contributors_url
+                slurp))))
+
+(defonce borkdude-repos
+  (-> "https://api.github.com/users/borkdude/repos"
+      (client/get {:oauth-token (System/getenv "GITHUB_AUTH_TOKEN")})
+      :body
+      (charred/read-json :key-fn keyword)
+      (->> (map :html_url))))
+
+(defonce babashka-repos
+  (-> "https://api.github.com/users/babashka/repos"
+      (client/get {:oauth-token (System/getenv "GITHUB_AUTH_TOKEN")})
+      :body
+      (charred/read-json :key-fn keyword)
+      (->> (map :html_url))))
--- projects/london-clojurians-dec-2023-talk/src/notebooks/explore.clj
+++ projects/london-clojurians-dec-2023-talk/src/notebooks/explore.clj
@@ -1,12 +1,93 @@
 (ns notebooks.explore
-  (:require [scicloj.clay.v2.config :as config]))
+  (:require [data.generate-dataset]
+            [scicloj.kindly.v4.kind :as kind]
+            [scicloj.noj.v1.vis :as vis]
+            [aerial.hanami.templates :as ht]
+            [tablecloth.api :as tc]
+            [clojure.string :as string]
+            [tech.v3.datatype.datetime :as datetime]))
 
-(+ 1 2)
+(def processed-data
+  (-> (->> data.generate-dataset/data
+           (mapcat (comp :items :items))
+           (map #(select-keys % [:created_at :html_url :open_issues_count :watchers :forks_count])))
+      tc/dataset
+      (tc/map-columns :user [:html_url]
+                      (fn [html-url]
+                        (-> html-url
+                            (string/split #"/")
+                            (->> (drop 3)
+                                 first))))
+      (tc/map-columns :clojure-user [:user]
+                      (partial = "clojure"))
+      (tc/map-columns :babashka-user [:user]
+                      (partial = "babashka"))))
 
-;; # A1
+(-> processed-data
+    (tc/group-by [:user])
+    (tc/aggregate {:n tc/row-count})
+    (tc/order-by [:n] :desc))
 
-;; ## B1
+(-> processed-data
+    (tc/select-rows #(and (-> % :forks_count (> 0))
+                          (-> % :open_issues_count (> 0))))
+    (vis/hanami-plot ht/point-chart
+                     {:X "open_issues_count"
+                      :Y "forks_count"
+                      :XSCALE {:type "log"}
+                      :YSCALE {:type "log"}
+                      :COLOR "babashka-user"
+                      :SIZE "watchers"})
+    (assoc-in [:encoding :size :type] "quantitative")
+    (assoc-in [:encoding :size :scale :type] "log"))
 
-;; ## B2
 
-;; # A2
+(->> data.generate-dataset/data
+     (mapcat (comp :items :items))
+     first)
+
+(-> processed-data
+    (vis/hanami-plot ht/bar-chart
+                     {:X "created_at"
+                      :XTYPE "temporal"
+                      :Y "watchers"}))
+
+(-> processed-data
+    (tc/map-columns :yearmonth
+                    [:created_at]
+                    #(subs % 0 7)))
+
+(-> processed-data
+    (tc/map-columns :yearmonth
+                    [:created_at]
+                    #(subs % 0 7))
+    (tc/group-by [:yearmonth])
+    (tc/aggregate {:n tc/row-count})
+    (tc/order-by [:yearmonth]))
+
+
+
+(-> processed-data
+    (tc/map-columns :yearmonth
+                    [:created_at]
+                    #(subs % 0 7))
+    (tc/group-by [:yearmonth])
+    (tc/aggregate {:n tc/row-count})
+    (tc/order-by [:yearmonth])
+    (vis/hanami-plot ht/bar-chart
+                     {:X "yearmonth"
+                      :XTYPE "temporal"
+                      :Y "n"}))
+
+(-> processed-data
+    (tc/head 100)
+    (tc/map-columns :yearmonth
+                    [:created_at]
+                    #(subs % 0 7))
+    (tc/group-by [:yearmonth])
+    (tc/aggregate {:n tc/row-count})
+    (tc/order-by [:yearmonth])
+    (vis/hanami-plot ht/bar-chart
+                     {:X "yearmonth"
+                      :XTYPE "temporal"
+                      :Y "n"}))
