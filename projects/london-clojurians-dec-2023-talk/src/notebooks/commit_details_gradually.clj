16498633ae63b0a9929b8865da3e5295cc022876
daslu <daniel.slutsky@gmail.com>
2023-12-12 15:43:39 +0200

    some progress with the gradual commit analysis

--- projects/london-clojurians-dec-2023-talk/src/notebooks/commit_details_gradually.clj
+++ projects/london-clojurians-dec-2023-talk/src/notebooks/commit_details_gradually.clj
@@ -14,82 +14,150 @@
             [clojure.string :as string]
             [clojure.java.shell :as shell]
             [scicloj.kind-portal.v1.api :as kind-portal]
-            [portal.api :as portal]))
+            [portal.api :as portal]
+            [clojure.java.io :as io]
+            [scicloj.kind-portal.v1.api :as kind-portal]))
 
 
-(def *commit-details
-  (atom []))
 
 
-(defn current-commit-details []
-  (apply tc/concat @*commit-details))
+(defn url->clone-path [url]
+  (-> url
+      (string/replace #"^https://github.com/" "")
+      (string/replace #"/" "__")
+      (->> (str "/workspace/clones-for-gradual-analysis/"))))
+
+(defn clone! [{:keys [html_url clone-path]}]
+  (io/make-parents clone-path)
+  (when-not (-> clone-path
+                io/file
+                (.exists))
+    (prn [:git-clone html_url clone-path])
+    (shell/sh "git"
+              "clone"
+              html_url
+              clone-path)))
 
 
-(defn current-report []
-  (-> (current-commit-details)
-      (tc/group-by [:language :html_url])
-      (tc/aggregate {:n-contributors (fn [ds]
-                                       (-> ds
-                                           :email
-                                           distinct
-                                           count))})
-      (tc/order-by [:n-contributors] :desc)
-      (tc/left-join data.generate-dataset/repos-ds [:html_url])
-      (tc/select-rows #(-> % :n-contributors (> 20)))
-      (tc/add-columns {:log-stargazers #(-> %
-                                            :stargazers_count
-                                            fun/log)
-                       :log-contributors #(-> %
-                                              :n-contributors
-                                              fun/log)})
-      (stats/add-predictions :log-stargazers
-                             [:log-contributors]
-                             {:model-type :smile.regression/ordinary-least-square})
-      (hanami/linear-regression-plot
-       :log-stargazers
-       :log-contributors
-       {:X :log-contributors
-        :XSCALE {:zero false}
-        :YSCALE {:zero false
-                 :domain [5 12]}
-        :line-options {:MCOLOR "brown"}})))
+(defn clone-all! [])
 
-(defn collect-repo-commit-details! [{:as repo
-                                     :keys [language html_url]}]
-  (prn [:git-log html_url])
-  (swap! *commit-details
-         conj (-> html_url
-                  data.generate-dataset/url->clone-path
-                  (->> (format "--git-dir=%s/.git"))
-                  (#(shell/sh "git" %
-                              "log"
-                              "--pretty=format:\"%ad,%ae\""
-                              "--date=short"))
-                  :out
-                  (string/replace #"\"" "")
-                  string/split-lines
-                  (->> (map (fn [line]
-                              (-> line
-                                  (string/split #",")
-                                  ((fn [[date email]]
-                                     (merge repo
-                                            {:date date
-                                             :email email})))))))
-                  tc/dataset)))
+(defn git-log [{:as repo
+                :keys [html_url clone-path]}]
+  (prn [:git-log clone-path])
+  (-> clone-path
+      (->> (format "--git-dir=%s/.git"))
+      (#(shell/sh "git" %
+                  "log"
+                  "--pretty=format:\"%ad,%ae\""
+                  "--date=short"))
+      :out
+      (string/replace #"\"" "")
+      string/split-lines
+      (->> (map (fn [line]
+                  (-> line
+                      (string/split #",")
+                      ((fn [[date email]]
+                         (merge repo
+                                {:date date
+                                 :email email})))))))
+      tc/dataset
+      (tc/set-dataset-name html_url)))
 
-(defn collect-commit-details! []
-  (reset! *commit-details [])
-  (->> data.generate-dataset/repos
-       (filter #(-> % :language (= "Clojure")))
-       (partition 100)
-       (run! (fn [batch]
-               (->> batch
-                    (run! collect-repo-commit-details!))
-               (kind-portal/kindly-submit-context
-                {:value (kind/hiccup
-                         [:h1 (count @*commit-details) " repos handled"])})
-               (kind-portal/kindly-submit-context
-                {:value (current-report)})))))
+
+(def *stop (atom false))
+(def *raw-commit-logs (atom []))
+(def *portal-commit-logs (atom []))
+
+(defn collect! [limit]
+  (prn [:DBG1])
+  (reset! *stop false)
+  (prn [:DBG2])
+  (reset! *raw-commit-logs [])
+  (prn [:DBG2])
+  (reset! *portal-commit-logs [])
+  (prn [:DBG3])
+  (-> data.generate-dataset/repos-ds
+      (tc/map-columns :clone-path [:html_url]
+                      url->clone-path)
+      (tc/select-rows #(and (-> % :size (< 100000))
+                            (-> % :language (= "Clojure"))))
+      (tc/rows :as-maps)
+      (->> (take limit)
+           (mapv (fn [repo]
+                   (prn repo)
+                   (when-not @*stop
+                     (clone! repo)
+                     (let [log (git-log repo)]
+                       (swap! *raw-commit-logs
+                              conj log)
+                       (swap! *portal-commit-logs
+                              #(-> %
+                                   (conj (kind-portal/prepare
+                                          {:value log}))
+                                   reverse))))))))
+  (prn [:stopped]))
 
 (comment
-  (collect-commit-details!))
+  (future (collect! 200))
+
+  (reset! *stop true)
+
+  (portal/open)
+  (portal/submit *portal-commit-logs))
+
+
+(defn report []
+  (let [raw-commit-logs @*raw-commit-logs
+        n (count raw-commit-logs)]
+    [(kind/hiccup
+      [:h1 n " repos"])
+     (when (> n 10)
+       (-> raw-commit-logs
+           (->> (apply tc/concat))
+           (tc/group-by [:language :html_url])
+           (tc/aggregate {:n-contributors (fn [ds]
+                                            (-> ds
+                                                :email
+                                                distinct
+                                                count))})
+           (tc/order-by [:n-contributors] :desc)
+           (tc/left-join data.generate-dataset/repos-ds [:html_url])
+           (tc/select-rows #(-> % :n-contributors (> 20)))
+           (tc/add-columns {:log-stargazers #(-> %
+                                                 :stargazers_count
+                                                 fun/log)
+                            :log-contributors #(-> %
+                                                   :n-contributors
+                                                   fun/log)})
+           (stats/add-predictions :log-stargazers
+                                  [:log-contributors]
+                                  {:model-type :smile.regression/ordinary-least-square})
+           (hanami/linear-regression-plot
+            :log-contributors
+            :log-stargazers
+            {:XSCALE {:zero false
+                      :domain [5 12]}
+             :YSCALE {:zero false}
+             :point-options {:MSIZE 100}
+             :line-options {:MCOLOR "brown"
+                            :MSIZE 10
+                            :OPACITY 0.5}})
+           (assoc-in [:encoding :tooltip]
+                     {:field "html_url"})))]))
+
+(def *report (atom nil))
+
+(defn update-report! []
+  (prn :report-update)
+  (reset! *report (kind-portal/prepare
+                   {:value (report)})))
+
+(comment
+  (update-report!)
+
+  (add-watch *raw-commit-logs
+             :report
+             (fn [_ _ _ _]
+               (update-report!)))
+
+  (portal/submit *report))
