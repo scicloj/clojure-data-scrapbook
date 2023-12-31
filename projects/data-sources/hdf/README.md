4eee389fbab264b7006cfc04e8aaaf4f0d654b5d
daslu <daniel.slutsky@gmail.com>
2023-12-31 22:39:28 +0200

    updated HDF tutorial for the current workflow

--- projects/data-sources/hdf/README.md
+++ projects/data-sources/hdf/README.md
@@ -1,4 +1,4 @@
 
 # hdf
 
-Reading files of the HDF format
+A tutorial for reading files of the HDF format using [JHDF](https://github.com/jamesmudd/jhdf)
--- projects/data-sources/hdf/notebooks/scicloj/scrapbook/hdf.clj
+++ projects/data-sources/hdf/notebooks/scicloj/scrapbook/hdf.clj
@@ -1,3 +1,22 @@
+^{:kindly/hide-code? true
+  :kindly/kind :kind/hiccup}
+[:table
+ [:tr
+  [:td "This is part of the Scicloj "
+   [:a {:href "https://scicloj.github.io/clojure-data-scrapbook/"}
+    "Clojure Data Scrapbook"]
+   "."]
+  [:a
+   {:href "https://scicloj.github.io/clojure-data-scrapbook/"}
+   [:img {:src "https://scicloj.github.io/sci-cloj-logo-transparent.png"
+          :alt "SciCloj logo"
+          :width "40"
+          :align "left"}]]]]
+
+;; # Reading HDF files
+
+;; Original discussion at the Clojurians Zulip [chat](https://scicloj.github.io/docs/community/chat/): [#data-science > import hdf files](https://clojurians.zulipchat.com/#narrow/stream/151924-data-science/topic/import.20hdf.20files).
+
 (ns scicloj.scrapbook.hdf
   (:require [babashka.fs :as fs]
             [tech.v3.tensor :as tensor]
@@ -8,9 +27,12 @@
   (:import io.jhdf.HdfFile
            java.io.File))
 
-;; # Processing HDF files
-
 (set! *warn-on-reflection* true)
+
+;; We will use the followig function
+;; to read an [HDF](https://en.wikipedia.org/wiki/Hierarchical_Data_Format) file
+;; using [jHDF](https://github.com/jamesmudd/jhdf/),
+;; and convert it to [dtype-next](https://github.com/cnuernber/dtype-next) tensors.
 
 (defn hdf5->tensors [path]
   (let [file ^File (io/file path)
@@ -27,12 +49,16 @@
                               .getData
                               tensor/->tensor)}))))))
 
+;; Let us apply the function to a test file:
+
 (def tensors
   (hdf5->tensors "data/test.h5"))
 
 (count tensors)
 
 (take 3 tensors)
+
+;; Let us visualize a few of the tensors as images:
 
 (->> tensors
      (take 3)
