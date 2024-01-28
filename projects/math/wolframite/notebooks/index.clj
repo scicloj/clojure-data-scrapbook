10a466aec39959cf17315a65baa13d330bfd7cf8
daslu <daniel.slutsky@gmail.com>
2024-01-28 19:20:07 +0200

    Wolframite - minor fixes

--- projects/math/wolframite/notebooks/index.clj
+++ projects/math/wolframite/notebooks/index.clj
@@ -5,7 +5,8 @@
 ;; It is also appears at the Wolrfamite repo as [dev/kindly-demo.clj](https://github.com/scicloj/wolframite/blob/main/dev/kindly_demo.clj).
 
 ;; Note that to use Wolframite, you need Wolfamite in the dependencies,
-;; as well as the `WOLFRAM_INSTALL_PATH`
+;; and if Wolfraimte fails to find the correct install path automatically,
+;; you may need to have the `WOLFRAM_INSTALL_PATH` environment variable
 ;; set up in your system, as explained in
 ;; [Wolframite's README](https://github.com/scicloj/wolframite/).
 
@@ -28,8 +29,6 @@
 (def md
   (comp kindly/hide-code kind/md))
 
-(md "## Make sure Wo")
-
 (md "## Init (base example)")
 ^:note-to-test/skip
 
@@ -39,7 +38,7 @@
 
 (wl/eval "{1 , 2, 3} . {4, 5, 6}")
 
-(md "## Def // intern WL fns, i.e. effectively define WL fns as clojure fns:")
+(md "## Def / intern WL fns, i.e. effectively define WL fns as clojure fns:")
 
 (def W:Plus (parse/parse-fn 'Plus {:kernel/link @wl/kernel-link-atom}))
 
@@ -52,7 +51,7 @@
 
 (greetings "Stephen")
 
-(md " ## Bidirectional translation
+(md "## Bidirectional translation
 (Somewhat experimental, especially in the wl->clj direction)")
 
 
