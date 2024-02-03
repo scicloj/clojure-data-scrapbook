e88ce329e6d6673263d8c273b5189742baec6539
daslu <daniel.slutsky@gmail.com>
2024-02-03 23:53:47 +0200

    Noj - progress w/ ggplot

--- projects/noj/notebooks/ggplot.clj
+++ projects/noj/notebooks/ggplot.clj
@@ -3,7 +3,7 @@
 ;; # Exploring ggplot
 
 (ns ggplot
-  (:require [clojisr.v1.r :as r :refer [r]]
+  (:require [clojisr.v1.r :as r :refer [r r$ r->clj]]
             [clojisr.v1.applications.plotting :as plotting]
             [scicloj.kindly.v4.kind :as kind]))
 
@@ -20,4 +20,23 @@
     kind/html)
 
 (-> plot
-    r/r->clj)
+    r->clj
+    (dissoc :data))
+
+
+;; Trying to mimic [cxplot](https://cxplot.com/index.html)'s internal ggplot.as.list (WIP):
+(let [f (-> plot
+            (r$ 'facet)
+            (r$ 'params)
+            (r$ 'facets))]
+  (when (-> `(is.null ~f)
+            r
+            r->clj)
+    (let [facet (-> `(ls ~f)
+                    r
+                    (r->clj)
+                    first)]
+      {:facet facet
+       :facet-levels (-> plot
+                         (r$ 'data)
+                         (r/bra facet))})))
