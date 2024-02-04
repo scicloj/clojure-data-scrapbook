3254b2f3e1bc811f2ad33c0bdfe5f9c1cbaeb6b8
daslu <daniel.slutsky@gmail.com>
2024-02-05 00:06:27 +0200

    Noj - ggplot - progress w/ extracting data

--- projects/noj/notebooks/ggplot.clj
+++ projects/noj/notebooks/ggplot.clj
@@ -53,38 +53,50 @@
    (->clj r-obj avoid []))
   ([r-obj avoid path]
    (prn path)
-   (or (some-> (some->> r-obj
-                        r.base/names
-                        r->clj
-                        (filter (complement avoid))
-                        (map (fn [nam]
-                               [(keyword nam) (-> r-obj
-                                                  (r$ nam)
-                                                  (->clj avoid
-                                                         (conj path nam)))]))
-                        (into {})))
-       (if (-> r-obj
-               r.base/class
-               r->clj
-               first
-               (= "ggproto_method"))
-         :ggproto-method)
-       (if (-> r-obj
-               r.base/is-list
-               r->clj
-               first)
-         (-> r-obj
-             r.base/length
-             r->clj
-             first
-             range
-             (->> (mapv (fn [i]
-                          (prn [path (inc i)])
-                          (-> r-obj
-                              (r/brabra (inc i))
-                              (->clj avoid
-                                     (conj path [i])))))))
-         r-obj))))
+   (let [relevant-names (some->> r-obj
+                                 r.base/names
+                                 r->clj
+                                 (filter (complement avoid)))]
+     (cond
+       ;;
+       ;; a named list or a ggproto object
+       (seq relevant-names) (->> relevant-names
+                                 (map (fn [nam]
+                                        [(keyword nam) (-> r-obj
+                                                           (r$ nam)
+                                                           (->clj avoid
+                                                                  (conj path nam)))]))
+                                 (into {}))
+       ;;
+       ;; a ggproto method
+       (-> r-obj
+           r.base/class
+           r->clj
+           first
+           (= "ggproto_method"))
+       :ggproto-method
+       ;;
+       ;; an unnamed list
+       (-> r-obj
+           r.base/is-list
+           r->clj
+           first)
+       (-> r-obj
+           r.base/length
+           r->clj
+           first
+           range
+           (->> (mapv (fn [i]
+                        (prn [path (inc i)])
+                        (-> r-obj
+                            (r/brabra (inc i))
+                            (->clj avoid
+                                   (conj path [i])))))))
+       ;;
+       (r.base/is-function r-obj) :function
+       (r.base/is-atomic r-obj) (r->clj r-obj)
+       :else r-obj))))
+
 
 (-> plot
     (->clj #{"data" "plot_env"}))
