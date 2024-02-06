ff1373ab63785234feb25877788796e6454df0c0
daslu <daniel.slutsky@gmail.com>
2024-02-06 23:56:14 +0200

    updated the make_docs script

--- make_docs.clj
+++ make_docs.clj
@@ -7,6 +7,9 @@
 ;; Please do not use it if you do not need to.
 ;; Note that it erases some directories.
 
+(shell/sh "mv" "projects/index/docs/index.html" "docs/")
+(shell/sh "mv" "projects/index/docs/index_files" "docs/")
+
 (->> "projects"
      (shell/sh "find")
      :out
