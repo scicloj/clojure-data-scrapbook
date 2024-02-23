f4d5985d0f1ee4056e8b69cbaf28a997a1da63f6
daslu <daniel.slutsky@gmail.com>
2024-02-23 23:58:34 +0200

    ggplot - rerendered

--- projects/datavis/ggplot/notebooks/dev.clj
+++ projects/datavis/ggplot/notebooks/dev.clj
@@ -5,7 +5,8 @@
              :base-source-path "notebooks"
              :source-path ["index.clj"
                            "using_clojisr.clj"
-                           "representing.clj"]
+                           ;; "representing.clj"
+                           ]
              :base-target-path "docs"
              :book {:title "Exploring ggplot"}
              :clean-up-target-dir true})
