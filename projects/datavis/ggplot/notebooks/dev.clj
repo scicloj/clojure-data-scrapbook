6180a07c7599071acb9e5ed395822f58ded3aa84
daslu <daniel.slutsky@gmail.com>
2024-02-25 23:29:21 +0200

    added header

--- projects/datavis/ggplot/notebooks/dev.clj
+++ projects/datavis/ggplot/notebooks/dev.clj
@@ -7,6 +7,7 @@
                            "using_clojisr.clj"
                            "representing.clj"
                            "comparing.clj"
+                           "study_session_20240225.clj"
                            "draft.clj"]
              :base-target-path "docs"
              :book {:title "Exploring ggplot"}
