048d55ca43a0768a35375842e9600cd28d34b686
daslu <daniel.slutsky@gmail.com>
2024-04-01 01:07:03 +0300

    Contributing guide

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -28,13 +28,15 @@
 
 ;; There are two ways to contribute to the scrapbook. Both are encouraged, depending on your convenience.
 
-;; A. Using your own repo
-;; - Create a repo with a [Clay](https://scicloj.github.io/clay/) notebook, multiple notebooks, or a book. Add a link to the scrapbook at the top of your notebooks. For an example repo, see [noj-getting-started](https://github.com/scicloj/noj-getting-started/tree/main).
-;; - Create a [Pull Request](https://github.com/scicloj/clojure-data-scrapbook/pulls) adding it at the bottom of the [table of contents EDN file](https://github.com/scicloj/clojure-data-scrapbook/blob/main/notebooks/toc.edn).
+;; ### Using your own repo
 
-;; B. Updating the scrapbook repo
-;; - Please discuss it first with us, so that we think about the appropirate location in the repo.
-;; - Add (or update) notebooks or books under the scrapbook's [repo](https://github.com/scicloj/clojure-data-scrapbook/) itself. Begin your commit messages with the name of the relevant notebook or book. E.g., "Random Forest Tutorial - added data visualizations".
+;; 1. Create a repo with a [Clay](https://scicloj.github.io/clay/) notebook, multiple notebooks, or a book. Add a link to the scrapbook at the top of your notebooks. For an example repo, see [noj-getting-started](https://github.com/scicloj/noj-getting-started/tree/main).
+;; 2. Create a [Pull Request](https://github.com/scicloj/clojure-data-scrapbook/pulls) adding it at the bottom of the [table of contents](https://github.com/scicloj/clojure-data-scrapbook/blob/main/notebooks/toc.edn).
+
+;; ### Updating the scrapbook repo
+
+;; 1. Please discuss it with us first. Together, we will consider the appropirate locations, etc.
+;; 2. Create a [Pull Request](https://github.com/scicloj/clojure-data-scrapbook/pulls) adding (or updating) notebooks or books under the scrapbook's [repo](https://github.com/scicloj/clojure-data-scrapbook/), and updating the [table of contents](https://github.com/scicloj/clojure-data-scrapbook/blob/main/notebooks/toc.edn). Begin your commit messages with the name of the relevant notebook or book. E.g., "Random Forest Tutorial - added data visualizations".
 
 ;; ## Contents
 
