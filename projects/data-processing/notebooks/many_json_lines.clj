51c4939651924c6419df8b92cf70de1a271af9e7
daslu <daniel.slutsky@gmail.com>
2024-04-18 23:56:33 +0300

    many-json-lines - WIP

--- projects/data-processing/notebooks/many_json_lines.clj
+++ projects/data-processing/notebooks/many_json_lines.clj
@@ -16,28 +16,34 @@
   (:require [charred.api :as charred]
             [scicloj.kindly.v4.kind :as kind]
             [clojure.java.io :as io]
-            [clojure.java.shell :as shell]))
+            [clojure.java.shell :as shell]
+            [clojure.string :as str]))
 
 ;; Let us create a file with many JSON lines, a so-called JSONL file.
 
-(def path "/tmp/lines.jsonl")
+(def path "/workspace/lines.jsonl")
 
-(def n-lines 1000000)
+(def n-lines 500000000)
 
-(with-open [writer ((charred/json-writer-fn {}) path)]
-  (dotimes [i n-lines]
-    (.writeObject writer
-                  {:i i
-                   :x (rand)})
-    ;; Here we use an internal implementation detail of the JSONWriter
-    ;; object created by Charred - it holds a regular Writer inside.
-    ;; We will use it for the newline character.
-    (.write (.w writer)
-            "\n")))
+#_(defonce run-once-1
+    (with-open [writer ((charred/json-writer-fn {}) path)]
+      (dotimes [i n-lines]
+        (.writeObject writer
+                      {:i i
+                       :x (rand)
+                       :y (rand)})
+        ;; Here we use an internal implementation detail of the JSONWriter
+        ;; object created by Charred - it holds a regular Writer inside.
+        ;; We will use it for the newline character.
+        (.write (.w writer)
+                "\n"))))
 
 ;; We can check how big it is.
 
-(:out (shell/sh "wc" path))
+(-> (shell/sh "ls" "-lh" path)
+    :out
+    (str/split #" ")
+    (nth 4))
 
 ;; Let us look at a few of those lines we wrote.
 
@@ -66,11 +72,19 @@
 
 (def small-n 100)
 
-(with-open [json-lines (json-supplier)]
-  (->> json-lines
+(with-open [json-fn (json-supplier)]
+  (->> json-fn
        (map extract-relevant-data)
        (take small-n)
        (reduce +)))
+
+;; Alternatively:
+
+(with-open [json-fn (json-supplier)]
+  (transduce (comp (map extract-relevant-data)
+                   (take small-n))
+             +
+             json-fn))
 
 ;; This result makes sense: we sum up random numbers
 ;; which were sampled uniformely between 0 and 1,
@@ -78,5 +92,107 @@
 
 (* 0.5 small-n)
 
+;; Now let us go about processing the whole file.
 ;; Lazy sequences keep all the data in memory.
 ;; Let us use our supplier in a more memory-efficient way.
+
+#_(time
+   (with-open [json-fn (json-supplier)]
+     (loop [sum 0
+            line-data (.get json-fn)]
+       (if-let [x (extract-relevant-data line-data)]
+         (recur (+ sum x)
+                (.get json-fn))
+         (do (.close json-fn)
+             sum)))))
+
+
+(defn allocated-memory-MB []
+  (let [r (Runtime/getRuntime)]
+    (/ (- (.totalMemory r)
+          (.freeMemory r))
+       1000000.0)))
+
+
+(with-open [json-fn (json-supplier)]
+  (let [_ (System/gc)
+        baseline-memory (allocated-memory-MB)
+        s (->> json-fn
+               seq
+               (take 1000000))]
+    [(reduce + (map extract-relevant-data s))
+     (allocated-memory-MB)]))
+
+
+(with-open [json-fn (json-supplier)]
+  (let [_ (System/gc)
+        baseline-memory (allocated-memory-MB)
+        s (->> json-fn
+               seq
+               (take 1000000))]
+    [(reduce + (map extract-relevant-data s))
+     (count s)
+     (allocated-memory-MB)]))
+
+(let [_ (System/gc)
+      baseline-memory (allocated-memory-MB)
+      s (->> rand
+             (repeatedly 10000000))]
+  [(reduce + s)
+   (count s)
+   (- (allocated-memory-MB) baseline-memory)])
+
+
+(defn allocated-memory-MB []
+  (let [r (Runtime/getRuntime)]
+    (/ (- (.totalMemory r)
+          (.freeMemory r))
+       1000000.0)))
+
+(defn measure-sequence-consumption-seq [summary-fn]
+  (let [_ (System/gc)
+        baseline-memory (allocated-memory-MB)
+        s (take 100000000 (repeat 1))]
+    {:summary (summary-fn s)
+     :memory-usage-MB (- (allocated-memory-MB) baseline-memory)}))
+
+[(repeatedly
+  5
+  #(measure-sequence-consumption-seq
+    (fn [s]
+      (reduce + s))))
+ (repeatedly
+  5
+  #(measure-sequence-consumption-seq
+    (fn [s]
+      [(reduce + s)
+       (count s)])))
+ (repeatedly
+  5
+  #(measure-sequence-consumption-seq
+    (juxt count count)))]
+
+
+
+
+(defn measure-sequence-consumption-jsonl [summary-fn]
+  (let [_ (System/gc)
+        baseline-memory (allocated-memory-MB)
+        s (->> (json-supplier)
+               (take 1000000)
+               (map extract-relevant-data))]
+    {:summary (summary-fn s)
+     :memory-usage-MB (- (allocated-memory-MB) baseline-memory)}))
+
+
+[(repeatedly
+  5
+  #(measure-sequence-consumption-jsonl
+    (fn [s]
+      [(reduce + s)
+       (count s)])))
+ (repeatedly
+  5
+  #(measure-sequence-consumption-jsonl
+    (fn [s]
+      (reduce + s))))]
