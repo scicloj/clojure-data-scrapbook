c194d25e4c3e48b164b5cb7161c69ce04e16227e
daslu <daniel.slutsky@gmail.com>
2024-07-23 23:52:57 +0300

    python plotting - impl draft

--- projects/datavis/python/plotting/notebooks/index.clj
+++ projects/datavis/python/plotting/notebooks/index.clj
@@ -1,3 +1,39 @@
 (ns index
   (:require [libpython-clj2.require :refer [require-python]]
             [libpython-clj2.python :refer [py. py.. py.-] :as py]))
+
+
+;; inspiration: http://gigasquidsoftware.com/blog/2020/01/18/parens-for-pyplot/
+
+(require-python 'matplotlib.pyplot
+                'matplotlib.backends.backend_agg
+                'numpy)
+
+(defmacro with-pyplot
+  "Takes forms with mathplotlib.pyplot and returns a showable (SVG) plot."
+  [& body]
+  `(let [_# (matplotlib.pyplot/clf)
+         fig# (matplotlib.pyplot/figure)
+         agg-canvas# (matplotlib.backends.backend_agg/FigureCanvasAgg fig#)
+         path# (:path (tempfiles/tempfile! ".svg"))]
+     ~(cons 'do body)
+     (py. agg-canvas# "draw")
+     (matplotlib.pyplot/savefig path#)
+     (-> path#
+         slurp
+         kind/html)))
+
+
+(defn pyplot
+  "Takes a function plotting using mathplotlib.pyplot, and returns a showable (SVG) plot"
+  [plotting-function]
+  (let [_ (matplotlib.pyplot/clf)
+        fig (matplotlib.pyplot/figure)
+        agg-canvas (matplotlib.backends.backend_agg/FigureCanvasAgg fig)
+        path (:path (tempfiles/tempfile! ".svg"))]
+    (plotting-function)
+    (py. agg-canvas "draw")
+    (matplotlib.pyplot/savefig path)
+    (-> path
+        slurp
+        kind/html)))
