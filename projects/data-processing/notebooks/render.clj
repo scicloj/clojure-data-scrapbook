a7c9806c03360a74150f652a0c9fbb6d10a60037
Carsten Behring <carsten.behring@gmail.com>
2024-09-02 09:09:52 +0200

    added devcontainer and render script

---
+++ projects/data-processing/notebooks/render.clj
@@ -0,0 +1,12 @@
+(ns dev
+  (:require [scicloj.clay.v2.api :as clay]))
+
+(clay/make! {:format [:quarto :html]
+             :base-source-path "notebooks"
+             :source-path ["index.clj"
+                           "many_json_lines.clj"]
+             :base-target-path "docs"
+             :book {:title "Data processing"}
+             :clean-up-target-dir true
+             :show false})
+(System/exit 0)
